1cb4f006697254765795fceb30ff60a30d7c8172
Angelo Yin <angelo.yin@gmail.com>
2014-11-17 22:17:55 +0800

    chapter01

---
+++ std-c-lib/chapter01/assert.h
@@ -0,0 +1,12 @@
+#undef assert
+
+#ifdef NDEBUG
+	#define assert(test)	((void)0)
+#else
+	void _Assert(char *);
+
+	#define _STR(x) _VAL(x)
+	#define _VAL(x) #x
+	#define assert(test)	((test) ? (void)0 \
+		 : _Assert(FILE__":"_STR(__LINE__) " " #test))
+#endif
---
+++ std-c-lib/chapter01/tassert.c
@@ -0,0 +1,43 @@
+#define NDEBUG
+#include <assert.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static int val = 0;
+
+static void field_abort(int sig)
+{
+	if (val == 1)
+	{
+		puts("SUCCESS testing <assert.h>");
+		exit(EXIT_SUCCESS);
+	}
+	else
+	{
+		puts("FAILURE testing <assert.h>");
+		exit(EXIT_FAILURE);
+	}
+}
+
+static void dummy()
+{
+	int i = 0;
+	assert(i == 0);
+	assert(i == 1);
+}
+
+#undef NDEBUG
+#include <assert.h>
+
+int main()
+{
+	assert(signal(SIGABRT, &field_abort) != SIG_ERR);
+	dummy();
+	assert(val == 0);
+	++val;
+	fputs("Sample assertion failure message ==\n", stderr);
+	assert(val == 0);
+	puts("FAILURE testing <assert.h>");
+	return (EXIT_FAILURE);
+}

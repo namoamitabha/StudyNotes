1a948831585514af96ae4cb8af031f0076d8386c
Angelo Yin <angelo.yin@gmail.com>
2014-11-21 09:53:43 +0800

    impl islower and isprint

--- std-c-lib/chapter02/ctype0.h
+++ std-c-lib/chapter02/ctype0.h
@@ -13,7 +13,7 @@
 #define _XD	0x01 /* '0'-'9', 'A'-'F', 'a'-'f'*/
 
 int isalnum0(int), isalpha0(int), iscntrl0(int), isdigit0(int);
-int isgraph0(int);
+int isgraph0(int), islower0(int), isprint0(int);
 /*
 int isalnum(int), isalpha(int), iscntrl(int), isdigit(int);
 int isgraph(int), islower(int), isprint(int), ispunct(int);
@@ -32,9 +32,11 @@ extern const short *_Ctype; //, *_Tolower, *_Toupper;
 #define isdigit0(c) (_Ctype[(int)(c)] & (_DI))
 
 #define isgraph0(c) (_Ctype[(int)(c)] & (_DI|_LO|_PU|_UP|_XA))
-/* 
-#define islower(c) (_Ctype[(int)c] & (_LO))
-#define isprint(c) (_Ctype[(int)c] & (_DI|_LO|_PU|_SP|_UP|_XA))
+
+#define islower0(c) (_Ctype[(int)(c)] & (_LO))
+
+#define isprint0(c) (_Ctype[(int)(c)] & (_DI|_LO|_PU|_SP|_UP|_XA))
+/*
 #define ispunct(c) (_Ctype[(int)c] & (_PU))
 #define isspace(c) (_Ctype[(int)c] & (_CN|_SP|_XS))
 #define isupper(c) (_Ctype[(int)c] & (_UP))
--- std-c-lib/chapter02/ctype_tests.c
+++ std-c-lib/chapter02/ctype_tests.c
@@ -2,36 +2,57 @@
 #include <gtest/gtest.h>
 #include <limits.h>
 
-TEST(CType, isalnum) {
+TEST(CType, isalnum)
+{
 	ASSERT_TRUE((isalnum0)((int)'0'));
 	ASSERT_TRUE(isalnum0((int)'0'));
 }
 
-TEST(CType, isalpha) {
+TEST(CType, isalpha)
+{
 	ASSERT_TRUE(isalpha0((int)'a'));
 	ASSERT_TRUE((isalpha0)((int)'a'));
 }
 
-TEST(CType, iscntrl0) {
+TEST(CType, iscntrl)
+{
 	ASSERT_TRUE(iscntrl0('\a'));
 	ASSERT_TRUE(iscntrl0('\b'));
 }
 
-TEST(CType, isdigit) {
+TEST(CType, isdigit)
+{
 	char *s;
+
 	for (s = "0123456789"; *s; ++s) {
 		ASSERT_TRUE(isdigit0((int)*s));
 		ASSERT_TRUE((isdigit0)((int)*s));
 	}
 }
 
-TEST(CType, isgraph) {
+TEST(CType, isgraph)
+{
 	ASSERT_TRUE(isgraph0('0'));
 	ASSERT_TRUE((isgraph0)('0'));
 }
 
-TEST(CType, ClassMembership) {
+TEST(CType, islower)
+{
+	ASSERT_TRUE(islower0('a'));
+	ASSERT_FALSE(islower0('A'));
+	ASSERT_TRUE((islower0)('a'));
+	ASSERT_FALSE((islower0)('A'));
+}
+
+TEST(CType, isprint)
+{
+	ASSERT_TRUE(isprint0('&'));
+}
+
+TEST(CType, ClassMembership)
+{
 	int c;
+
 	for (c = EOF; c <= UCHAR_MAX; ++c) {
 		if ((isdigit)(c))
 			ASSERT_TRUE((isalnum)(c));

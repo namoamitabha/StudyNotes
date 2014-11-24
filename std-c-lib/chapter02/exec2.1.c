07025fc723d08f2d63a6abd9f6ac7db9ccb22e8c
Angelo Yin <angelo.yin@gmail.com>
2014-11-24 13:33:13 +0800

    fix style issue of exec2.1.c

--- std-c-lib/chapter02/exec2.1.c
+++ std-c-lib/chapter02/exec2.1.c
@@ -1,55 +1,45 @@
-
 #include "ctype0.h"
 #include <stdio.h>
 
-int main()
+int main(void)
 {
 	char *s;
+
 	for (s = "Hello, world!\n"; *s; ++s) {
 		printf("%c:\n", *s);
-		if (!isalnum0(*s)) {
+		if (!isalnum0(*s))
 			printf("isalnum\n");
-		}
 
-		if (!isalpha0(*s)) {
+		if (!isalpha0(*s))
 			printf("isalpha\n");
-		}
 
-		if (!iscntrl0(*s)) {
+		if (!iscntrl0(*s))
 			printf("iscntrl\n");
-		}
 
-		if (!isdigit0(*s)) {
+		if (!isdigit0(*s))
 			printf("isdigit\n");
-		}
 
-		if (!isgraph0(*s)) {
+		if (!isgraph0(*s))
 			printf("isgraph\n");
-		}
 
-		if (!islower0(*s)) {
+		if (!islower0(*s))
 			printf("islower\n");
-		}
 
-		if (!isprint0(*s)) {
+		if (!isprint0(*s))
 			printf("isprint\n");
-		}
 
-		if (!ispunct0(*s)) {
+		if (!ispunct0(*s))
 			printf("ispunct\n");
-		}
 
-		if (!isspace0(*s)) {
+		if (!isspace0(*s))
 			printf("isspace\n");
-		}
 
-		if (!isupper0(*s)) {
+		if (!isupper0(*s))
 			printf("isupper\n");
-		}
 
-		if (!isxdigit0(*s)) {
+		if (!isxdigit0(*s))
 			printf("isxdigit\n");
-		}
+
 		printf("\n********\n");
 	}
 }

bf1ef23ca99f3be4b64aae7ad20451a66c8ed30e
Angelo Yin <angelo.yin@gmail.com>
2014-12-05 10:27:03 +0800

    register_chrdev_region_test

---
+++ ldd3/chapter03/register_chrdev_region_test.c
@@ -0,0 +1,54 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/sched.h>
+#include <linux/version.h>
+#include <linux/vermagic.h>
+
+#include <linux/types.h>
+#include <linux/kdev_t.h>
+
+#include <linux/fs.h>
+
+/* MODULE_LICENSE("Dual BSD/GPL"); */
+MODULE_LICENSE("GPL v2");
+MODULE_AUTHOR("Angelo Yin");
+MODULE_DESCRIPTION("Hello world experiment");
+MODULE_VERSION("1.0.0");
+MODULE_ALIAS("AngeloHello");
+
+static int register_chrdev_region_init(void)
+{
+	pr_alert("Hello, world\n");
+	pr_alert("The process is \"%s\" (pid %i)\n",
+	       current->comm, current->pid);
+
+	pr_alert("UTS_RELEASE:%s", UTS_RELEASE);
+	/* pr_alert("LINUX_VERSION_CODE:%d", LINUX_VERSION_CODE); */
+	pr_alert("KERNEL_VERSION:%d", KERNEL_VERSION(2, 6, 10));
+
+	dev_t first = 0;
+	unsigned int count = 5;
+	char *name = "register_chrdev_region_test";
+	int error;
+
+	error = register_chrdev_region(first, count, name);
+	if (!error)
+		pr_alert("register chardev region successful");
+	else
+		pr_alert("register chardev region failed. error:%d", error);
+
+	return 0;  
+}
+
+static void register_chrdev_region_exit(void)
+{
+	pr_alert("Goodbye, beautiful world\n");
+}
+
+void export_test(void)
+{
+}
+EXPORT_SYMBOL(export_test);
+
+module_init(register_chrdev_region_init);
+module_exit(register_chrdev_region_exit);

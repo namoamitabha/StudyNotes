3ff0c7136a94d87a142caea92382e3b6ed9dd92c
Angelo Yin <angelo.yin@gmail.com>
2014-12-12 07:31:15 +0800

    delete blank lines

--- ldd3/chapter03/firstdev/firstdev.c
+++ ldd3/chapter03/firstdev/firstdev.c
@@ -34,6 +34,7 @@ static void firstdev_exit(void);
 loff_t fdev_llseek(struct file *filp, loff_t off, int whence)
 {
 	pr_alert("fdev_llseek");
+
 	return 0;
 }
 
@@ -43,6 +44,7 @@ ssize_t fdev_read(struct file *filp, char __user *buf, size_t count,
 	struct firstdev *dev = filp->private_data;
 
 	pr_alert("fdev_read, major=%d, minor=%d", dev->major, dev->minor);
+
 	return 0;
 }
 
@@ -52,6 +54,7 @@ ssize_t fdev_write(struct file *filp, const char __user *buf, size_t count,
 	struct firstdev *dev = filp->private_data;
 
 	pr_alert("fdev_write, major=%d, minor=%d", dev->major, dev->minor);
+
 	return 0;
 }
 
@@ -67,7 +70,6 @@ int fdev_open(struct inode *inode, struct file *filp)
 
 	dev = container_of(inode->i_cdev, struct firstdev, cdev);
 	filp->private_data = dev;
-
 	pr_alert("fdev_open, major=%d, minor=%d", dev->major, dev->minor);
 
 	return 0;
@@ -78,6 +80,7 @@ int fdev_release(struct inode *inode, struct file *filp)
 	struct firstdev *dev = filp->private_data;
 
 	pr_alert("fdev_release, major=%d, minor=%d", dev->major, dev->minor);
+
 	return 0;
 }
 
@@ -99,10 +102,8 @@ static int firstdev_init(void)
 	pr_alert("DEVICE:%s\n", name);
 	pr_alert("The process is \"%s\" (pid %i)\n",
 	       current->comm, current->pid);
-
 	pr_alert("UTS_RELEASE:%s", UTS_RELEASE);
 	pr_alert("KERNEL_VERSION:%d", KERNEL_VERSION(2, 6, 10));
-
 
 	unsigned int firstminor = 0;
 	int err;
@@ -115,8 +116,6 @@ static int firstdev_init(void)
 	} else {
 		pr_alert("alloc_chrdev_region failed.");
 	}
-
-
 
 	firstdev_p = kmalloc_array(count, sizeof(struct firstdev), GFP_KERNEL);
 	if (!firstdev_p) {
@@ -132,7 +131,6 @@ static int firstdev_init(void)
 	int i, major, devno;
 
 	major = MAJOR(dev);
-
 	for (i = 0; i < count; ++i) {
 		struct firstdev *p = &firstdev_p[i];
 
@@ -166,11 +164,8 @@ static void firstdev_exit(void)
 		kfree(firstdev_p);
 		pr_alert("kfree(firstdev_p);");
 	}
-
 	unregister_chrdev_region(dev, count);
-
 	pr_alert("unregister_chrdev_region(first, count);");
-
 	pr_alert("Goodbye, beautiful world\n");
 }
 

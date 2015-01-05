4c73a7ef1167f63c4d17c60c9c7f2bfbb019387d
Angelo Yin <angelo.yin@gmail.com>
2015-01-05 14:04:24 +0800

    dlist_ins_next

--- master-algorithm-with-c/dlist/dlist.c
+++ master-algorithm-with-c/dlist/dlist.c
@@ -18,16 +18,18 @@ int dlist_ins_next(DList *list, DListElmt *element, const void *data)
 {
 	DListElmt *newElmt = (DListElmt *)malloc(sizeof(DListElmt));
 
-	if (newElmt == NULL)
+	if (NULL == newElmt)
 		return -1;
 
 	newElmt->data = (void *)data;
 
 	if (element == NULL) {
-		list->head = newElmt;
-		list->tail = newElmt;
-		newElmt->next = NULL;
+		newElmt->next = list->head;
 		newElmt->prev = NULL;
+		list->head = newElmt;
+
+		if (NULL == dlist_tail(list))
+			list->tail = newElmt;
 	} else {
 		if (dlist_is_tail(element))
 			list->tail = newElmt;
@@ -35,8 +37,34 @@ int dlist_ins_next(DList *list, DListElmt *element, const void *data)
 		newElmt->next = element->next;
 		newElmt->prev = element;
 		element->next = newElmt;
-
 	}
 	++list->size;
+	return 0;
+}
+
+int dlist_ins_prev(DList *list, DListElmt *element, const void *data)
+{
+	DListElmt *newElmt = (DListElmt *)malloc(sizeof(DListElmt));
+
+	if (NULL == newElmt)
+		return -1;
+
+	newElmt->data = (void *)data;
+
+	if (NULL == element || dlist_is_head(element)) {
+		newElmt->next = list->head;
+		newElmt->prev = NULL;
+
+		list->head = newElmt;
+
+		if (NULL == dlist_tail(list))
+			list->tail = newElmt;
+	} else {
+		newElmt->prev = element->prev;
+		element->prev = newElmt;
+		newElmt->next = element;
+	}
+	++list->size;
+
 	return 0;
 }
--- master-algorithm-with-c/dlist/dlist_tests.c
+++ master-algorithm-with-c/dlist/dlist_tests.c
@@ -7,6 +7,19 @@ void destroy(void *data)
 
 }
 
+void print_list(DList *list)
+{
+	int i = 0;
+
+	DListElmt *current = list->head;
+
+	while (NULL != current) {
+		printf("%d:%d\n", ++i, *((int *)current->data));
+		current = current->next;
+	}
+}
+
+
 TEST(DList, dlist_init)
 {
 	DList *list = (DList *)malloc(sizeof(DList));
@@ -52,5 +65,58 @@ TEST(DList, dlist_ins_next)
 	EXPECT_EQ(0, result);
 	EXPECT_EQ(3, dlist_size(list));
 	EXPECT_EQ(*a, *((int *)dlist_head(list)->data));
-	EXPECT_EQ(*c, *((int *)list->head->next->data));	
+	EXPECT_EQ(*c, *((int *)list->head->next->data));
+
+	int *d = (int *)malloc(sizeof(int));
+
+	*d = 4;
+	result = dlist_ins_next(list, NULL, d);
+	EXPECT_EQ(0, result);
+	EXPECT_EQ(4, dlist_size(list));
+	EXPECT_EQ(*d, *((int *)dlist_head(list)->data));
+	EXPECT_EQ(*b, *((int *)dlist_tail(list)->data));
+
+	print_list(list);
+
+	dlist_destroy(list);
+	free(list);
+}
+
+TEST(DList, dlist_ins_prev)
+{
+	int result;
+
+	DList *list = (DList *)malloc(sizeof(DList));
+
+	dlist_init(list, destroy);
+	
+	int *a = (int *)malloc(sizeof(int));
+
+	*a = 1;
+	result = dlist_ins_prev(list, NULL, a);
+	EXPECT_EQ(0, result);
+	EXPECT_EQ(1, dlist_size(list));
+	EXPECT_EQ(*a, *((int *)dlist_head(list)->data));
+	EXPECT_EQ(*a, *((int *)dlist_tail(list)->data));
+
+	int *b = (int *)malloc(sizeof(int));
+
+	*b = 2;
+	result = dlist_ins_prev(list, dlist_head(list), b);
+	EXPECT_EQ(0, result);
+	EXPECT_EQ(2, dlist_size(list));
+	EXPECT_EQ(*b, *((int *)dlist_head(list)->data));
+	EXPECT_EQ(*a, *((int *)dlist_tail(list)->data));
+	print_list(list);
+
+	int *c = (int *)malloc(sizeof(int));
+
+	*c = 3;
+	result = dlist_ins_prev(list, dlist_tail(list), c);
+
+	print_list(list);
+	EXPECT_EQ(0, result);
+	EXPECT_EQ(*b, *((int *)dlist_head(list)->data));
+	EXPECT_EQ(*c, *((int *)list->head->next->data));
+	EXPECT_EQ(*a, *((int *)dlist_tail(list)->data));
 }

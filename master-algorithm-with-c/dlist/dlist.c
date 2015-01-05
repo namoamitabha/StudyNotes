a21788a7fd4b1736189bccfbcbd0410217f31000
Angelo Yin <angelo.yin@gmail.com>
2015-01-05 09:51:01 +0800

    dlist_ins_next

--- master-algorithm-with-c/dlist/dlist.c
+++ master-algorithm-with-c/dlist/dlist.c
@@ -9,3 +9,34 @@ void dlist_init(DList *list, void (*destroy)(void *data))
 	list->tail = NULL;
 	list->destroy = destroy;
 }
+
+void dlist_destroy(DList *list)
+{
+}
+
+int dlist_ins_next(DList *list, DListElmt *element, const void *data)
+{
+	DListElmt *newElmt = (DListElmt *)malloc(sizeof(DListElmt));
+
+	if (newElmt == NULL)
+		return -1;
+
+	newElmt->data = (void *)data;
+
+	if (element == NULL) {
+		list->head = newElmt;
+		list->tail = newElmt;
+		newElmt->next = NULL;
+		newElmt->prev = NULL;
+	} else {
+		if (dlist_is_tail(element))
+			list->tail = newElmt;
+
+		newElmt->next = element->next;
+		newElmt->prev = element;
+		element->next = newElmt;
+
+	}
+	++list->size;
+	return 0;
+}
--- master-algorithm-with-c/dlist/dlist_tests.c
+++ master-algorithm-with-c/dlist/dlist_tests.c
@@ -15,9 +15,42 @@ TEST(DList, dlist_init)
 	EXPECT_TRUE(NULL == dlist_head(list));
 	EXPECT_TRUE(NULL == dlist_tail(list));
 	EXPECT_TRUE(destroy == list->destroy);
+
+	dlist_destroy(list);
+	free(list);
 }
 
 TEST(DList, dlist_ins_next)
 {
+	int result;
 
+	DList *list = (DList *)malloc(sizeof(DList));
+	dlist_init(list, destroy);
+
+	int *a = (int *)malloc(sizeof(int));
+
+	*a = 1;
+	result = dlist_ins_next(list, NULL, a);
+	EXPECT_EQ(0, result);
+	EXPECT_EQ(1, dlist_size(list));
+	EXPECT_EQ(*a, *((int *)dlist_head(list)->data));
+	EXPECT_EQ(*a, *((int *)dlist_tail(list)->data));
+
+	int *b = (int *)malloc(sizeof(int));
+
+	*b = 2;
+	result = dlist_ins_next(list, list->head, b);
+	EXPECT_EQ(0, result);
+	EXPECT_EQ(2, dlist_size(list));
+	EXPECT_EQ(*a, *((int *)dlist_head(list)->data));
+	EXPECT_EQ(*b, *((int *)dlist_tail(list)->data));
+
+	int *c = (int *)malloc(sizeof(int));
+
+	*c = 3;
+	result = dlist_ins_next(list, list->head, c);
+	EXPECT_EQ(0, result);
+	EXPECT_EQ(3, dlist_size(list));
+	EXPECT_EQ(*a, *((int *)dlist_head(list)->data));
+	EXPECT_EQ(*c, *((int *)list->head->next->data));	
 }

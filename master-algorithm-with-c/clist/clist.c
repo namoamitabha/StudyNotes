a8ea7284a8dc10763aca059d2d05c6fdd15b8406
Angelo Yin <angelo.yin@gmail.com>
2015-01-06 19:13:17 +0800

    clist_init

--- master-algorithm-with-c/clist/clist.c
+++ master-algorithm-with-c/clist/clist.c
@@ -1,1 +1,8 @@
 #include "clist.h"
+
+void clist_init(CList *list, void (*destroy)(void *data))
+{
+	list->size = 0;
+	list->head = NULL;
+	list->destroy = destroy;
+}
--- master-algorithm-with-c/clist/clist_tests.c
+++ master-algorithm-with-c/clist/clist_tests.c
@@ -2,7 +2,23 @@
 #include <gtest/gtest.h>
 #include <stdlib.h>
 
+void destroy(void *data)
+{
+
+}
+
 TEST(CList, clist_init)
+{
+	CList *list = (CList *)malloc(sizeof(CList));
+	clist_init(list, destroy);
+	EXPECT_EQ(0, clist_size(list));
+	EXPECT_TRUE(NULL == list->head);
+	EXPECT_TRUE(destroy == list->destroy);
+
+	free(list);
+}
+
+TEST(CList, clist_ins_next)
 {
 
 }

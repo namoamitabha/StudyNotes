e65c6f5ac5a48fe34355966f8b9532e61489fe4e
Angelo Yin <angelo.yin@gmail.com>
2015-01-07 08:58:43 +0800

    clist_ins_next, clist_rem_next

--- master-algorithm-with-c/clist/clist.c
+++ master-algorithm-with-c/clist/clist.c
@@ -6,3 +6,55 @@ void clist_init(CList *list, void (*destroy)(void *data))
 	list->head = NULL;
 	list->destroy = destroy;
 }
+
+void clist_destroy(CList *list)
+{
+
+}
+
+int clist_ins_next(CList *list, CListElmt *element, const void *data)
+{
+	if (NULL == element && clist_size(list) > 0)
+		return -1;
+
+	CListElmt *newElmt = (CListElmt *)malloc(sizeof(CListElmt));
+
+	if (NULL == newElmt)
+		return -1;
+
+	newElmt->data = (void *)data;
+
+	if (0 == clist_size(list)) {
+		list->head = newElmt;
+		newElmt->next = list->head;
+	} else {
+		newElmt->next = element->next;
+		element->next = newElmt;
+	}
+
+	++list->size;
+	return 0;
+}
+
+int clist_rem_next(CList *list, CListElmt *element, void **data)
+{
+	if (element == NULL || 0 == clist_size(list))
+		return -1;
+
+	CListElmt *remElmt;
+
+	if (element == element->next) {
+		remElmt = clist_head(list);
+		*data = clist_data(remElmt);
+		list->head = NULL;
+	} else {
+		remElmt = element->next;
+		*data = clist_data(remElmt);
+		element->next = remElmt->next;
+	}
+
+	--list->size;
+	free(remElmt);
+
+	return 0;
+}
--- master-algorithm-with-c/clist/clist_tests.c
+++ master-algorithm-with-c/clist/clist_tests.c
@@ -7,6 +7,17 @@ void destroy(void *data)
 
 }
 
+void clist_print(CList *list)
+{
+	int i = 0;
+	CListElmt *current = clist_head(list);
+
+	while (i < clist_size(list)) {
+		printf("%d\n", *((int *)clist_data(current)));
+		current = current->next;
+		++i;
+	}
+}
 TEST(CList, clist_init)
 {
 	CList *list = (CList *)malloc(sizeof(CList));
@@ -15,10 +26,87 @@ TEST(CList, clist_init)
 	EXPECT_TRUE(NULL == list->head);
 	EXPECT_TRUE(destroy == list->destroy);
 
+	clist_destroy(list);
 	free(list);
 }
 
 TEST(CList, clist_ins_next)
 {
+	int result;
 
+	CList *list = (CList *)malloc(sizeof(CList));
+	clist_init(list, destroy);
+
+	int *a = (int *)malloc(sizeof(int));
+	*a = 1;
+	result = clist_ins_next(list, NULL, a);
+	EXPECT_EQ(0, result);
+	EXPECT_EQ(1, clist_size(list));
+	EXPECT_EQ(*a, *((int *)clist_data(clist_head(list))));
+	EXPECT_TRUE( clist_next(list->head) == clist_head(list));
+
+	int *b = (int *)malloc(sizeof(int));
+	*b = 2;
+
+	result = clist_ins_next(list, NULL, b);
+	EXPECT_EQ(-1, result);
+
+	result = clist_ins_next(list, clist_head(list), b);
+	EXPECT_EQ(0, result);
+	EXPECT_EQ(2, clist_size(list));
+	EXPECT_EQ(*a, *((int *)clist_data(clist_head(list))));
+	EXPECT_EQ(*b, *((int *)clist_data(clist_next(clist_head(list)))));
+	EXPECT_TRUE(clist_head(list) 
+		    == clist_next(clist_next(clist_head(list))));
+
+	int *c = (int *)malloc(sizeof(int));
+	*c = 3;
+	result = clist_ins_next(list, clist_head(list), c);
+	EXPECT_EQ(0, result);
+	EXPECT_EQ(3, clist_size(list));
+	EXPECT_EQ(*a, *((int *)clist_data(clist_head(list))));
+	EXPECT_EQ(*c, *((int *)clist_head(list)->next->data));
+	EXPECT_EQ(*b, *((int *)list->head->next->next->data));
+	EXPECT_TRUE(clist_head(list)
+		== list->head->next->next->next);
+
+	clist_destroy(list);
+	free(list);	
+}
+
+TEST(CList, clist_rem_next)
+{
+	int result;
+	void *data;
+	CList *list = (CList *)malloc(sizeof(CList));
+	clist_init(list, destroy);
+
+	result = clist_rem_next(list, NULL, &data);
+	EXPECT_EQ(-1, result);
+
+	int *a;
+	for (int i = 0; i < 3; ++i) {
+		a = (int *)malloc(sizeof(int));
+		*a = i;
+		clist_ins_next(list, clist_head(list), a);
+	}
+	clist_print(list);
+
+	result = clist_rem_next(list, clist_head(list), &data);
+	EXPECT_EQ(0, result);
+	EXPECT_EQ(2, clist_size(list));
+	EXPECT_EQ(2, *((int *)data));
+
+	result = clist_rem_next(list, clist_head(list), &data);
+	EXPECT_EQ(0, result);
+	EXPECT_EQ(1, clist_size(list));
+	EXPECT_EQ(1, *((int *)data));
+	EXPECT_TRUE(clist_head(list)
+		    == clist_head(list)->next);
+
+	result = clist_rem_next(list, clist_head(list), &data);
+	EXPECT_EQ(0, result);
+	EXPECT_EQ(0, clist_size(list));
+	EXPECT_EQ(0, *((int *)data));
+	EXPECT_TRUE(NULL == clist_head(list));
 }

e553b475d94151d1f4e9c47747c04aa0f358fd56
Angelo Yin <angelo.yin@gmail.com>
2015-01-08 13:21:58 +0800

    queue_enqueue & queue_dequeue

--- master-algorithm-with-c/queue/queue.c
+++ master-algorithm-with-c/queue/queue.c
@@ -1,2 +1,12 @@
 #include "queue.h"
 
+int queue_enqueue(Queue *queue, const void *data)
+{
+	return list_ins_next(queue, queue->tail, data);
+}
+
+
+int queue_dequeue(Queue *queue, void **data)
+{
+	return list_rem_next(queue, NULL, data);
+}
--- master-algorithm-with-c/queue/queue_tests.c
+++ master-algorithm-with-c/queue/queue_tests.c
@@ -4,7 +4,49 @@
 #include <limits.h>
 #include <stdlib.h>
 
+void destroy(void *data)
+{
+	free(data);
+}
+
 TEST(Queue, queue_enqueue_queue_dequeue)
 {
+	int result;
 
+	Queue *queue = (Queue *)malloc(sizeof(Queue));
+	queue_init(queue, destroy);
+
+	int *a = (int *)malloc(sizeof(int));
+	*a = 1;
+
+	result = queue_enqueue(queue, a);
+	EXPECT_EQ(0, result);
+	EXPECT_EQ(1, queue_size(queue));
+	EXPECT_EQ(*a, *((int *)queue_peek(queue)));
+
+	int *b = (int *)malloc(sizeof(int));
+	*b = 2;
+
+	result = queue_enqueue(queue, b);
+	EXPECT_EQ(0, result);
+	EXPECT_EQ(2, queue_size(queue));
+	EXPECT_EQ(*a, *((int *)queue_peek(queue)));
+
+	void *data;
+	result = queue_dequeue(queue, &data);
+	EXPECT_EQ(0, result);
+	EXPECT_EQ(1, queue_size(queue));
+	EXPECT_EQ(*a, *((int *)data));
+	EXPECT_EQ(*b, *((int *)queue_peek(queue)));
+
+	result = queue_dequeue(queue, &data);
+	EXPECT_EQ(0, result);
+	EXPECT_EQ(0, queue_size(queue));
+	EXPECT_EQ(*b, *((int *)data));
+
+	result = queue_dequeue(queue, &data);
+	EXPECT_EQ(-1, result);
+
+	queue_destroy(queue);
+	free(queue);
 }

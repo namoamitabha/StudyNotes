daa32214ed1b8e311e39c7e6c621c647b0370ac2
root <root@centos.angelo.com>
2015-01-21 09:43:04 +0800

    chtbl_init

--- master-algorithm-with-c/hash-table-chain/chtbl.c
+++ master-algorithm-with-c/hash-table-chain/chtbl.c
@@ -1,2 +1,19 @@
 #include "chtbl.h"
 
+int chtbl_init(CHTbl *htbl, int buckets,
+	       int (*h)(const void *key),
+	       int (*match)(const void *key1, const void *key2),
+	       void (*destroy)(void *data))
+{
+	htbl->table = (List *)malloc(sizeof(List) * buckets);
+	if (NULL == htbl->table)
+		return -1;
+
+	htbl->buckets = buckets;
+	htbl->size = 0;
+	htbl->h = h;
+	htbl->match = match;
+	htbl->destroy = destroy;
+
+	return 0;
+}
--- master-algorithm-with-c/hash-table-chain/chtbl_tests.c
+++ master-algorithm-with-c/hash-table-chain/chtbl_tests.c
@@ -2,7 +2,35 @@
 #include <gtest/gtest.h>
 #include <stdlib.h>
 
-TEST(CHTbl, chtbl_init)
+int h(const void *key)
+{
+	return 0;
+}
+
+int match(const void *key1, const void *key2)
+{
+	return 0;
+}
+
+void destroy(void *data)
 {
 
+}
+
+TEST(CHTbl, chtbl_init)
+{
+	int result;
+
+	CHTbl *htbl = (CHTbl *)malloc(sizeof(CHTbl));
+
+	result = chtbl_init(htbl, 10, h, match, destroy);
+	EXPECT_EQ(0, result);
+	EXPECT_EQ(0, htbl->size);
+	EXPECT_EQ(10, htbl->buckets);
+	EXPECT_TRUE(h == htbl->h);
+	EXPECT_TRUE(match == htbl->match);
+	EXPECT_TRUE(destroy == htbl->destroy);
+	EXPECT_TRUE(NULL != htbl->table);
+
+	free(htbl);
 }

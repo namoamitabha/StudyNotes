043ded572fef4476a45b5f31417fa7a71ae22977
Angelo Yin <angelo.yin@gmail.com>
2015-01-24 17:16:51 +0800

    ohtbl_init

--- master-algorithm-with-c/hash-table-open/ohtbl.c
+++ master-algorithm-with-c/hash-table-open/ohtbl.c
@@ -1,2 +1,33 @@
 #include "ohtbl.h"
+#include <string.h>
 
+int ohtbl_init(OHTbl *htbl, int positions,
+	       int (*h1)(const void *key),
+	       int (*h2)(const void *key),
+	       int (*match)(const void *key1, const void *key2),
+	       void (*destroy)(void *data))
+{
+	htbl->positions = positions;
+
+	htbl->h1 = h1;
+	htbl->h2 = h2;
+	htbl->match = match;
+	htbl->destroy = destroy;
+	htbl->size = 0;
+
+	htbl->table = (void **)malloc(sizeof(void *) * htbl->positions);
+	return 0;
+}
+
+void ohtbl_destroy(OHTbl *htbl)
+{
+	int i;
+	for (i = 0; i < htbl->positions; ++i) {
+		if (NULL != htbl->destroy)
+			htbl->destroy(*(htbl->table + i));
+	}
+
+	free(htbl->table);
+
+	memset(htbl, 0, sizeof(OHTbl));
+}
--- master-algorithm-with-c/hash-table-open/ohtbl_tests.c
+++ master-algorithm-with-c/hash-table-open/ohtbl_tests.c
@@ -2,7 +2,43 @@
 #include <gtest/gtest.h>
 #include <stdlib.h>
 
+static const int positions = 100;
+
+int h1(const void *key)
+{
+	return 0;
+}
+
+int h2(const void *key)
+{
+	return 0;
+}
+
+int match(const void *key1, const void *key2)
+{
+	return 0;
+}
+
+void destroy(void *data)
+{
+	free(data);
+}
+
 TEST(OHTbl, ohtbl_init)
 {
+	int result;
+	OHTbl *htbl = (OHTbl *)malloc(sizeof(OHTbl));
 
+	result = ohtbl_init(htbl, positions, h1, h2, match, destroy);
+	EXPECT_EQ(0, result);
+	EXPECT_TRUE(h1 == htbl->h1);
+	EXPECT_TRUE(h2 == htbl->h2);
+	EXPECT_TRUE(match == htbl->match);
+	EXPECT_TRUE(destroy == htbl->destroy);
+
+	EXPECT_EQ(0, ohtbl_size(htbl));
+	EXPECT_EQ(positions, htbl->positions);
+
+	/* ohtbl_destroy(htbl); */
+	free(htbl);
 }

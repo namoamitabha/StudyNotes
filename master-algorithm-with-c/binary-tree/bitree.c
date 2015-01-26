b196a587f2a5f6939a4b8fd9deb1eb77686573c4
Angelo Yin <angelo.yin@gmail.com>
2015-01-26 15:37:07 +0800

    bitree_init

--- master-algorithm-with-c/binary-tree/bitree.c
+++ master-algorithm-with-c/binary-tree/bitree.c
@@ -1,1 +1,8 @@
 #include "bitree.h"
+
+void bitree_init(BiTree *tree, void (*destroy)(void *data))
+{
+	tree->size = 0;
+	tree->destroy = destroy;
+	tree->root = NULL;
+}
--- master-algorithm-with-c/binary-tree/bitree_tests.c
+++ master-algorithm-with-c/binary-tree/bitree_tests.c
@@ -2,7 +2,21 @@
 #include <gtest/gtest.h>
 #include <stdlib.h>
 
+void destroy(void *data)
+{
+	if (NULL != data)
+		free(data);
+}
+
 TEST(BiTree, bitree_init)
 {
+	int result;
+	BiTree *tree = (BiTree *)malloc(sizeof(BiTree));
 
+	bitree_init(tree, destroy);
+	EXPECT_EQ(0, bitree_size(tree));
+	EXPECT_TRUE(NULL == bitree_root(tree));
+	EXPECT_TRUE(destroy == tree->destroy);
+
+	free(tree);
 }

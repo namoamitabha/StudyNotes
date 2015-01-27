67efb2bb318b19ad2c13e915b322c95f17e9f9ea
Angelo Yin <angelo.yin@gmail.com>
2015-01-27 09:06:06 +0800

    bitree_ins_left

--- master-algorithm-with-c/binary-tree/bitree_tests.c
+++ master-algorithm-with-c/binary-tree/bitree_tests.c
@@ -10,13 +10,42 @@ void destroy(void *data)
 
 TEST(BiTree, bitree_init)
 {
-	int result;
 	BiTree *tree = (BiTree *)malloc(sizeof(BiTree));
 
 	bitree_init(tree, destroy);
 	EXPECT_EQ(0, bitree_size(tree));
 	EXPECT_TRUE(NULL == bitree_root(tree));
 	EXPECT_TRUE(destroy == tree->destroy);
+
+	free(tree);
+}
+
+TEST(BiTree, bitree_ins_left)
+{
+	int result;
+	BiTree *tree = (BiTree *)malloc(sizeof(BiTree));
+
+	bitree_init(tree, destroy);
+
+	int i = 0;
+	result = bitree_ins_left(tree, NULL, &i);
+	EXPECT_EQ(0, result);
+	EXPECT_EQ(1, bitree_size(tree));
+	EXPECT_TRUE(NULL != bitree_root(tree));
+	EXPECT_EQ(i, *((int *)bitree_data(tree->root)));
+
+	int j = 1;
+	result = bitree_ins_left(tree, NULL, &j);
+	EXPECT_EQ(-1, result);
+
+	result = bitree_ins_left(tree, bitree_root(tree), &j);
+	EXPECT_EQ(0, result);
+	EXPECT_EQ(2, bitree_size(tree));
+	EXPECT_EQ(1, *((int *)(bitree_left(tree->root)->data)));
+
+	int k = 2;
+	result = bitree_ins_left(tree, bitree_root(tree), &k);
+	EXPECT_EQ(-1, result);
 
 	free(tree);
 }

f4713fd3698668ff3eec4ef178698778d3b2d6fd
root <root@centos.angelo.com>
2015-04-15 11:34:36 +0800

    dive into python3 - ch09 - implement toroman1 - pass

--- dive-into-python3/ch09/roman1.py
+++ dive-into-python3/ch09/roman1.py
@@ -1,12 +1,26 @@
 # roman1.py
 
+roman_numeral_map = (('M',  1000),
+                     ('CM', 900),
+                     ('D',  500),
+                     ('CD', 400),
+                     ('C',  100),
+                     ('XC', 90),
+                     ('L',  50),
+                     ('XL', 40),
+                     ('X',  10),
+                     ('IX', 9),
+                     ('V',  5),
+                     ('IV', 4),
+                     ('I',  1))
+
 def to_roman(n):
     '''convert integer to Roman numeral'''
+    result = ''
+    for numeral, integer in roman_numeral_map:
+        while n >= integer:
+            result += numeral
+            n -= integer
+            print('subtracting {0} from input, adding {1} to output'.format(integer, numeral))
 
-
-
-
-
-
-
-pass
+    return result
